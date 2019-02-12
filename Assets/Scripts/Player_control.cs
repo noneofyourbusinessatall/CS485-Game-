b9f19a593b2a193797d121495b4a11f1b7e6c3e2
anonymous <mspk7654@gmail.com>
2019-02-11 16:29:50 -0800

    third commit

---
+++ Assets/Scripts/Player_control.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_control : MonoBehaviour {
+	public float movementSpeed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotLeftRight = Input.GetAxis("Mouse X");
+		transform.Rotate(0, rotLeftRight, 0);
+
+		float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+
+		Vector3 speed = new Vector3( sideSpeed, 0, forwardSpeed );
+		CharacterController cc = GetComponent<CharacterController>();
+		cc.SimpleMove( speed );
+	}
+}
